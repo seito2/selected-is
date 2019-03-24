84b7452c9081f90c30c31d6a212f3a3332a0d8a2
hiraiseito <>
2019-03-24 12:37:49 +0900

    first

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const HelloWorld = {
+    template:
+    `
+    <div>
+        <p>Hellow, World</p>
+    </div>
+    `
+}
+
+const SelectedDrink = {
+    template:
+    `
+    <div id="selected-drink">
+        <select v-model="selected">
+            <option disabled value="">Please select one</option>
+            <option v-for="item in drink" @key="item">{{ item }}</option>
+        </select>
+        <span>選ばれたのは: {{ selected }}</span>
+    <div>
+    `,
+    data: function() {
+        return {
+            selected: '',
+            drink: ['綾鷹', '伊右衛門', '爽健美茶']
+        }
+    },
+}
+
+new Vue({
+    el: '#app',
+    components: {
+        'selected-drink': SelectedDrink
+    }
+})
